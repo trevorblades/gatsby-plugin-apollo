2b6b3b4562538c193892a3e6f56bfd407a03d022
Trevor Blades <tdblades@gmail.com>
2019-02-28 22:24:54 -0800

    Create the client

--- src/inject-provider.js
+++ src/inject-provider.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {ApolloClient} from 'apollo-client';
 import {ApolloProvider} from 'react-apollo';
 
 // eslint-disable-next-line react/prop-types
-export default function injectProvider({element}, {client}) {
+export default function injectProvider({element}, options) {
+  const client = new ApolloClient(options);
   return <ApolloProvider client={client}>{element}</ApolloProvider>;
 }
