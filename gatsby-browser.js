2a5b62e0e97dd5ab477c358eb2a9d1f845cdc16d
Trevor Blades <tdblades@gmail.com>
2019-08-23 20:23:20 -0700

    Add isomorphic fetch

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,10 @@
 import ApolloClient from 'apollo-boost';
 import React from 'react';
+import fetch from 'isomorphic-fetch';
 import {ApolloProvider} from '@apollo/react-common';
 
 // eslint-disable-next-line react/prop-types
 export const wrapRootElement = ({element}, pluginOptions) => {
-  const client = new ApolloClient(pluginOptions);
+  const client = new ApolloClient({...pluginOptions, fetch});
   return <ApolloProvider client={client}>{element}</ApolloProvider>;
 };
