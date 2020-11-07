3ebcba9f8ebd956dfad7d5af5b6b071643cb2107
Trevor Blades <tdblades@gmail.com>
2020-11-06 16:13:42 -0800

    Update deps, fix vuln

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,12 @@ const cache = new InMemoryCache();
 
 // eslint-disable-next-line react/prop-types
 export const wrapRootElement = ({element}, {uri, headers, credentials}) => {
-  const client = new ApolloClient({fetch, uri, headers, credentials, cache});
+  const client = new ApolloClient({
+    fetch,
+    uri,
+    headers,
+    credentials,
+    cache
+  });
   return <ApolloProvider client={client}>{element}</ApolloProvider>;
 };
