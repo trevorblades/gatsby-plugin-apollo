b9d2ba17bc94703415bc87fec9fa38d6141b809e
Trevor Blades <tdblades@gmail.com>
2021-11-27 21:36:57 -0800

    Support shadowing

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import client from './src/client';
 import fetch from 'isomorphic-fetch';
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 
-const cache = new InMemoryCache();
-
-// eslint-disable-next-line react/prop-types
-export const wrapRootElement = ({element}, {uri, headers, credentials}) => {
-  const client = new ApolloClient({
-    fetch,
-    uri,
-    headers,
-    credentials,
-    cache
-  });
-  return <ApolloProvider client={client}>{element}</ApolloProvider>;
-};
+export const wrapRootElement = ({element}, options) => (
+  <ApolloProvider
+    client={
+      client instanceof ApolloClient
+        ? client
+        : new ApolloClient({
+            ...options,
+            fetch,
+            cache: new InMemoryCache()
+          })
+    }
+  >
+    {element}
+  </ApolloProvider>
+);
